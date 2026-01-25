43c9b62ea8dbda98905756f507164a98ad8c6dc2
Yvon Perez <yvon.perez@etu.minesparis.psl.eu>
2026-01-25 20:27:15 +0100

    ajout des réseaux pour la politique et la valeur

--- train_ppo.py
+++ train_ppo.py
@@ -13,3 +13,41 @@ def encode_obs(obs: Dict[str, Any]) -> np.ndarray:
         1.0 if obs["done"] else 0.0,        # terminal ou pas
     ], dtype=np.float32)
 
+
+class PolicyNetwork(nn.Module):
+    
+    def __init__(self, obs_dim: int, act_dim: int, hidden: int = 256):
+        super().__init__()
+        
+        # Encodeur
+        self.encodeur = nn.Sequential(
+            nn.Linear(obs_dim,hidden),
+            nn.ReLU(),
+            nn.Sequential(hidden,hidden),
+            nn.ReLU()
+        )
+        
+        self.policy_head = nn.linear(hidden,act_dim)
+        
+        def forward(self, obs: torch.Tensor) -> torch.Tensor :
+            encoded = self.encodeur(obs)
+            action_logits = self.policy_head(encoded)
+            return action_logits
+        
+class ValueNetwork(nn.Module):
+    def __init__(self, obs_dim: int, hidden: int = 256):
+        super().__init__()
+        
+        self.network = nn.Sequential(
+            nn.linear(obs_dim,hidden),
+            nn.ReLU(),
+            nn.linear(hidden,hidden)
+            nn.ReLU()
+            nn.linear(hidden,1)
+        )
+        
+        def forward(self, obs: torch.Tensor) -> torch.Tensor : 
+            return self.network(obs)
+
+class AgentPPO :
+    v
